66a69aaae078508ac153e795f40383bebedbe606
Manoel <manoelivisson4@hotmail.com>
2026-03-30 15:12:28 -0300

    Retirando atributos não mais usados em trajetos e pontos de trajetos

--- app/models/trajeto.py
+++ app/models/trajeto.py
@@ -2,10 +2,9 @@ from config import db
 from typing import TYPE_CHECKING
 import datetime
 from sqlalchemy.orm import Mapped, mapped_column, relationship
-from sqlalchemy import String, ForeignKey, Integer
+from sqlalchemy import String
 
 if TYPE_CHECKING:
-  from models.veiculo import Veiculo
   from models.ponto_trajeto import PontoTrajeto
 
 class Trajeto(db.Model):
@@ -15,15 +14,6 @@ class Trajeto(db.Model):
   nome: Mapped[str] = mapped_column(String(50))
   horario_inicio: Mapped[datetime.time] = mapped_column()
   horario_final: Mapped[datetime.time] = mapped_column()
-  
-  # coluna de chave estrangeira
-  #veiculo_id: Mapped[int] = mapped_column(ForeignKey('veiculo.id'), nullable=True)
-
-  # relacionamento para acesso na via contrária
-  # se comunica com o relacionamento com Veiculo trajeto
-  # relacionamento 1 veiculo para N trajetos
-  # cada trajeto tem apenas 1 carro
-  #veiculo: Mapped['Veiculo'] = relationship(back_populates='trajeto')
 
   # relacionamento N pontos de trajeto para 1 trajeto
   # cada trajeto pode ter vários pontos associados
